51f6e8fcd214ab04fdc8aaddeacb79b1a9b40184
gninnorg <carsten@groenning.net>
2022-11-07 22:03:39 +0100

    take 2

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from temperature import get_wort_temperature
+import machine, onewire, ds18x20, time
 
-print(get_wort_temperature())
+ds_pin = machine.Pin(16)
+ds_pow = machine.Pin(17, machine.Pin.OUT)
+ds_pow.value(1)
 
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+roms = ds_sensor.scan()
+print('Found DS devices: ', roms)
+
+while True:
+  ds_sensor.convert_temp()
+  time.sleep_ms(750)
+  print(ds_sensor.read_temp(roms[0]))
+  time.sleep(5)
--- temperature.py
+++ temperature.py
@@ -1,27 +1,28 @@
 # https://randomnerdtutorials.com/micropython-ds18b20-esp32-esp8266/
 
-from config import PIN_DS18_PW, PIN_ONEWIRE
 from machine import Pin
-from onewire import OnWire
+from onewire import OneWire
 from ds18x20 import DS18X20
 from time import sleep_ms
 
-def get_wort_temperature():
-  # Turn on power to DS18
-  ds_pow = Pin(PIN_DS18_PW)
-  ds_pow.value(1)
+def get_wort_temperature(probe):
   
   # Open DS18
-  ds_pin = Pin(PIN_ONEWIRE)
-  ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+  ds_pin = Pin(probe)
+  ds_sensor = DS18X20(OneWire(ds_pin))
   roms = ds_sensor.scan()
   ds_sensor.convert_temp()
   sleep_ms(750)
 
   # Read temperature
+  temp = 0
   for rom in roms:
     temp = ds_sensor.read_temp(rom)
+    temp = ds_sensor.read_temp(rom)
+
     print(rom)
     print(temp)
+
+  ds_pin.value(0)
 
   return temp
